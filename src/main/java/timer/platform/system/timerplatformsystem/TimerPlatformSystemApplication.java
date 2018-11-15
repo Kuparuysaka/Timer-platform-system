6454faa252e93bd1e01c985b78585d4dd39d41ff
MAC <mac@MACdeMacBook-Pro.local>
2018-11-15 17:19:58 +0800

    创建repo

---
+++ src/main/java/timer/platform/system/timerplatformsystem/TimerPlatformSystemApplication.java
@@ -0,0 +1,12 @@
+package timer.platform.system.timerplatformsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerPlatformSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimerPlatformSystemApplication.class, args);
+    }
+}
